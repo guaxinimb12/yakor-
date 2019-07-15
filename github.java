750eac66d7c6934822418ebc38ceb4b4db28bf18
guaxinimb12 <52894783+guaxinimb12@users.noreply.github.com>
2019-07-15 12:53:26 -0300

    Add files via upload

---
+++ github.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+ // Check Examples
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+ 	public static void main(String args[])
+ 	{ 
+		System.out.println("Hello, Dcoder!");
+ 	}
+ }
